b53e8f9fca6025661863d59d89e98a9a9d4ba411
MCeban89 <ceban.mihail192@gmail.com>
2026-04-19 11:19:44 +0300

    initial commit

---
+++ src/test/java/org/example/roomreservation/RoomreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.roomreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomreservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
